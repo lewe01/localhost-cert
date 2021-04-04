47c1d40f4e0c426fcbfc34e9f8301f64a1243469
lewe01 <80843933+lewe01@users.noreply.github.com>
2021-04-04 17:12:42 +0300

    files

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const key = fs.readFileSync('../localhost/localhost.decrypted.key');
+const cert = fs.readFileSync('../localhost/localhost.crt');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Hello world!');
+});
+
+const https = require('https');
+const server = https.createServer({ key, cert }, app);
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is listening on https://localhost:${port}`);
+});
